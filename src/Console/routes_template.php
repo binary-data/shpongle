df70e24178c81c4ebf498ad8fed8b50e878e1b14
Anton Belonovich <tralick@gmail.com>
2021-01-25 21:47:46 +0400

    Remove leftover actions from route templates

--- src/Console/routes_template.php
+++ src/Console/routes_template.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Binarydata\Craftshop\Action\Index;
-use Binarydata\Craftshop\Action\Product;
 use FastRoute\RouteCollector;
 
 use function FastRoute\simpleDispatcher;
